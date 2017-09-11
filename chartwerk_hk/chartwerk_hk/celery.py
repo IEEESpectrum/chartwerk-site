7f935da45aa865bdc41354bde274a101b4a05e40
Josh Romero <josh@joshromero.com>
2017-09-11 11:15:46 -0500

    fix missing import and project name

--- chartwerk_hk/chartwerk_hk/celery.py
+++ chartwerk_hk/chartwerk_hk/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your project>.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chartwerk_hk.settings')
 
 app = Celery('chartwerk')
 app.config_from_object('django.conf:settings', namespace='CELERY')
