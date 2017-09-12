e4a74b6309665fd167cea0893de3541dc6f4cd51
Josh Romero <josh@joshromero.com>
2017-09-12 11:26:56 -0500

    add login/logout views and redirect homepage to app

--- chartwerk_hk/chartwerk_hk/urls.py
+++ chartwerk_hk/chartwerk_hk/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^chartwerk/', include('chartwerk.urls')),
+    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', logout),
+    url(r'^$', RedirectView.as_view(url='chartwerk/')),
 ]
