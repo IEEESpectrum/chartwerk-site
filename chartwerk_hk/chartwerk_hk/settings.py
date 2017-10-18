b8491a82a0c226c8906f9cdc47e6bbf55ca207b5
Josh Romero <josh@joshromero.com>
2017-10-18 16:45:45 -0500

    add config for color schemes and set default categorical scheme to IEEE Spectrum colors

--- chartwerk_hk/chartwerk_hk/settings.py
+++ chartwerk_hk/chartwerk_hk/settings.py
@@ -155,22 +155,114 @@ CHARTWERK_EMBED_TEMPLATE_CONTEXT = lambda chart: {
     'chart_path': 'https://s3.amazonaws.com/chartwerk.ieeespectrum.joshuarrr/charts/',
     'embed_script': 'https://s3.amazonaws.com/chartwerk.ieeespectrum.joshuarrr/main-embed.bundle.js',
 }
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#             'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
-#         },
-#     },
-# }
+CHARTWERK_COLOR_SCHEMES = {
+    'categorical': {
+        'default': [
+            '#03A6E3',
+            '#D40A06',
+            '#F75F08',
+            '#F5CD09',
+            '#48B44C',
+            '#682590',
+            '#3FBEB6',
+            '#C9C9C9',
+        ],
+    },
+    'sequential': {
+        'red': [
+            '#f5c8c1',
+            '#eea89e',
+            '#e8887a',
+            '#e26856',
+            '#dc4730',
+            '#c13621',
+            '#9d2c1b',
+            '#700f00',
+        ],
+        'blue': [
+            '#cfebff',
+            '#a5dbff',
+            '#7bc2f2',
+            '#5cb5f2',
+            '#359fe6',
+            '#2487c9',
+            '#0267ab',
+            '#004a7a',
+        ],
+        'green': [
+            '#d9ebc3',
+            '#bde388',
+            '#a2cf63',
+            '#7bc049',
+            '#65a835',
+            '#4e941b',
+            '#327303',
+            '#245400',
+        ],
+        'warm': [
+            '#ffe261',
+            '#ffc226',
+            '#ffa310',
+            '#f57f00',
+            '#e35000',
+            '#cc3300',
+            '#a31600',
+            '#700f00',
+        ],
+        'cool': [
+            '#d4f2cb',
+            '#ace6b1',
+            '#8bd9b9',
+            '#5ac9c1',
+            '#2cb9c7',
+            '#0c94c2',
+            '#0275c2',
+            '#004a7a',
+        ],
+    },
+    'diverging': {
+        'redBlue': [
+            '#0078d1',
+            '#299aee',
+            '#5ab5fa',
+            '#99d3ff',
+            '#f7a699',
+            '#f57864',
+            '#e34e36',
+            '#c42c14',
+        ],
+        'redBlueMix': [
+            '#0064c2',
+            '#2985f2',
+            '#7e94f7',
+            '#aea4fc',
+            '#d99ee8',
+            '#de7cbf',
+            '#ce5269',
+            '#ba230b',
+        ],
+        'redGreen': [
+            '#c12e17',
+            '#ea652b',
+            '#fc9943',
+            '#fcc857',
+            '#bee16d',
+            '#99cf51',
+            '#6ebe34',
+            '#539f1e',
+        ],
+        'orangePurple': [
+            '#b35806',
+            '#e08214',
+            '#fdb863',
+            '#fee0b6',
+            '#d8daeb',
+            '#b2abd2',
+            '#8073ac',
+            '#542788',
+        ],
+    }
+}
 
 LOGGING = {
     'version': 1,
